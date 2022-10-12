d67f9365da718051a59cad2abd91450321697d89
Julius Rummel <32172513+DerWaldie@users.noreply.github.com>
2022-10-12 23:04:51 +0200

    updated setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,9 +1,15 @@
-#!/bin/bash
+@echo off
 
-echo "Installing requirements..."
-npm install -g node-windows
-cd ./server/ & npm install & cd ../
-cd ./frontend/ & npm install & cd ../
+CALL echo "Installing requirements..."
+CALL npm install -g node-windows
 
-echo "Setup services to enable autostart"
-node ./setup_services.js
+echo "Installing server dependencies..."
+cd ./server/ & CALL npm install & cd ../
+
+echo "Installing frontend dependencies..."
+cd ./frontend/ & CALL npm install & cd ../
+
+CALL echo "Setup services to enable autostart"
+CALL node ./setup_services.js
+
+pause
