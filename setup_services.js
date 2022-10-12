228605c2267037bdebfc84b209bb5d730d084a86
Julius Rummel <32172513+DerWaldie@users.noreply.github.com>
2022-10-12 22:32:35 +0200

    created service file

--- setup_services.js
+++ setup_services.js
@@ -1,14 +1,29 @@
 var Service = require('node-windows').Service;
 
+// setup backend server
 var serverService = new Service({
-  name:'questlog matrix server',
-  description: 'The nodejs.org example web server.',
-  script: require('path').join(__dirname,'server/start_server.sh'),
-  workingDirectory: require('path').join(__dirname,'server/'),
+    name: 'questlog_matrix_server',
+    description: 'server for the maxtrix backend',
+    script: require('path').join(__dirname, 'server/index.js'),
+    workingDirectory: require('path').join(__dirname, 'server/')
 });
 
-serverService.on('install',function(){
-  svc.start();
+serverService.on('install', function () {
+    serverService.start();
 });
 
 serverService.install();
+
+// setup frontend server
+var frontendService = new Service({
+    name: 'questlog_matrix_frontend',
+    description: 'frontend for the maxtrix backend',
+    script: require('path').join(__dirname, 'frontend/index.js'),
+    workingDirectory: require('path').join(__dirname, 'frontend/')
+});
+
+frontendService.on('install', function () {
+    frontendService.start();
+});
+
+frontendService.install();
